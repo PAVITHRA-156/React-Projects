39de9ec5c673bac85919171d1c35bc28997eb8fa
PAVITHRA-156 <727723eucs156@skcet.ac.in>
2024-08-05 16:19:32 +0530

    Add files via upload

---
+++ day1/Cy2.js
@@ -0,0 +1,9 @@
+import 'React' from 'react'
+
+function Add()
+{
+    directions=direction.map(direction => direction);
+    document.write(directions);
+    <div>
+        <button onClick={;}></button>
+    </div>
